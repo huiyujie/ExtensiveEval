2afeb1f8e1d268781c413e7ee9a5bab75c6bbcb6
huiyujie <huiyujie0105@gmail.com>
2023-08-22 12:45:11 -0400

    ML

---
+++ ML/regressor.py
@@ -0,0 +1,27 @@
+from sklearn.metrics import r2_score
+from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import StandardScaler
+
+
+def standarize(train, valid):
+    global scaler
+    scaler = StandardScaler()
+    scaler.fit(train)
+    return scaler.transform(train), scaler.transform(valid)
+
+def process_data(train, valid, para_names):
+    x_train = train[para_names]
+    x_valid = valid[para_names]
+    y_train = train.iloc[:, -1].to_numpy()
+    y_valid = valid.iloc[:, -1].to_numpy()
+    x_train, x_valid = standarize(x_train, x_valid)
+
+    return x_train, y_train, x_valid, y_valid
+
+def MLP_regression(train, valid, para_names):
+    x_train, y_train, x_valid, y_valid = process_data(train, valid, para_names)
+    regr = MLPRegressor(solver="lbfgs", hidden_layer_sizes=(10,10,5),learning_rate_init=1e-1, random_state=1, max_iter=10000).fit(x_train, y_train)
+    y_pred = regr.predict(x_valid)
+    r2 = r2_score(y_valid, y_pred)
+    return r2
+
