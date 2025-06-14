d912745d5b5d4512762c310c951514ac115a24c5
sam1016yu <miaoyu1016@gmail.com>
2025-06-13 23:03:26 -0600

    modify run_all

--- run_all.sh
+++ run_all.sh
@@ -2,7 +2,7 @@
 
 system=$1
 
-methods=("random" "stratified" "neyman" "dist-aware" "balance")
+methods=("stratified" "neyman" "dist-aware" "balance")
 for method in "${methods[@]}"; do
     echo "Running analysis for method: $method"
     if ! /anova_venv/bin/python run_ml_analysis.py \
